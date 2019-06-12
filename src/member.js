c818500fa3e3ab9a05f1c7de963f80d1f13ca103
seokki <braveosk@gmail.com>
2019-06-12 18:19:37 +0900

    add: Markup Table

---
+++ src/member.js
@@ -0,0 +1,6 @@
+export const member = [
+  { name: '오석기', age: 29, members: []},
+  { name: '우학제', age: 29, members: ['황재현', '김윤섭', '안민호']},
+  { name: '박종혁', age: 26, members: ['류건', '정세종']},
+  { name: '손정우', age: 29, members: ['김정한', '박태준', '최성민']},
+]
