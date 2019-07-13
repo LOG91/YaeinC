400f81b4807ab9465ea748503e250ad76fde3ecb
log91 <peter1636@naver.com>
2019-07-14 00:33:37 +0900

    modify: chage port 9000 to 8080

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -15,7 +15,7 @@ module.exports = {
     index: "index.html",
     disableHostCheck: true,
     historyApiFallback: true,
-    port: 9000,
+    port: 8080,
     proxy: {
       '/api': {
           target: 'http://localhost:5000',
@@ -66,59 +66,3 @@ module.exports = {
     new CleanWebpackPlugin()
   ]
 }
-
-
-// const path = require("path");
-// const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const CleanWebpackPlugin = require("clean-webpack-plugin");
-
-// module.exports = {
-//   entry: "./src/index.js",
-//   output: {
-//     filename: "bundle.js",
-//     path: path.resolve(__dirname, "../build")
-//   },
-//   mode: "development",
-//   devServer: {
-//     contentBase: path.resolve(__dirname, "../build"),
-//     index: "index.html",
-//     port: 9000
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: "/node_modules",
-//         use: ["babel-loader"]
-//       },
-//       {
-//         test: /\.html$/,
-//         use: [
-//           {
-//             loader: "html-loader",
-//             options: { minimize: true }
-//           }
-//         ]
-//       },
-//       {
-//         test: /\.css$/,
-//         use: [MiniCssExtractPlugin.loader, "css-loader"]
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebPackPlugin({
-//       template: "./public/index.html",
-//       filename: "index.html"
-//     }),
-//     new MiniCssExtractPlugin({
-//       filename: "style.css"
-//     }),
-//     new CleanWebpackPlugin()
-//   ]
-// };
